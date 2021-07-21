558b0831b8feb9ed7b89260cf8a3210932502f6f
Jonathan Rugama <39573472+JonathanRugama@users.noreply.github.com>
2021-07-20 20:24:32 -0600

    Initial Commit

---
+++ SerializeAndDeserialize/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+{
+
+}
+namespace SerializeAndDeserialize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola");
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            Console.WriteLine(jsonString);
+        }
+    }
+}
---
+++ SerializeAndDeserialize/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeAndDeserialize
+{
+    class WeatherForecast
+    {
+        public DateTimeOffset Date { get; set; }
+
+        public int TemperatureCelsius { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
